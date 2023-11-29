dbedf2a9699da6193d7665bedfefb512c789213b
lin <tiffanyye912@gmail.com>
2023-11-29 21:32:34 +0100

    new Function: popup Tabelle.  Nach dem click von einem Routennummer, Zweite Tabelle mit Info wird gezeigt. click x: Man kann die Tabelle schließen. click button "Route anzeigen": es springt zu eine neue Seite.

--- public/stylesheets/Route.js
+++
@@ -1,7 +0,0 @@
-const openModal =document.querySelector('.open-button')
-const closeModal =document.querySelector('.close-button')
-const modal=document.querySelector('.modal')
-
-openModal.addEventListener('click', ()=>{
-    modal.showModal();
-})
---
+++ public/stylesheets/Routen.js
@@ -0,0 +1,14 @@
+
+const modal=document.querySelector('#modal');
+const closeModal=document.querySelector('.close-button');
+const openModal=document.querySelector('.press');
+
+openModal.addEventListener('click', ()=> {
+    modal.showModal();
+})
+openModal.addEventListener('click', ()=> {
+    modal.showModal();
+})
+closeModal.addEventListener('click', ()=> {
+    modal.close();
+})
