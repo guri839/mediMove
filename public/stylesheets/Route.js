7929a8c7dcb2daab54fc9d3b7fe148051a35bbeb
lin <tiffanyye912@gmail.com>
2023-11-29 10:01:36 +0100

    Lyn 3-ed commit, Routen Tabelle.

---
+++ public/stylesheets/Route.js
@@ -0,0 +1,7 @@
+const openModal =document.querySelector('.open-button')
+const closeModal =document.querySelector('.close-button')
+const modal=document.querySelector('.modal')
+
+openModal.addEventListener('click', ()=>{
+    modal.showModal();
+})
