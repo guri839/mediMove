f23105752ac32d467c033cb093b25da2db9a00ae
Lars Weiß <88539466+MichlCoding@users.noreply.github.com>
2023-12-21 11:24:00 +0100

    test and logging, how it works is described in the README

--- server.js
+++ server.js
@@ -1,30 +1,80 @@
+const winston = require('winston');
 const express = require("express");
 const app = express();
 
-app.use(express.static( 'public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/main.html');
+// Creates Logger for Logging into files
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({filename: 'error.log', level: 'error'}),
+        new winston.transports.File({filename: 'combined.log'}),
+    ],
 });
 
-app.get('/title', function(req, res) {
+// Transports logs to the console
+logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+}));
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/main.html');
+    logger.log({
+        level: 'info',
+        message: '"/" requested'
+    });
 });
-app.get('/routen', function(req, res) {
+
+app.get('/title', function (req, res) {
+    res.sendFile(__dirname + '/public/main.html');
+    logger.log({
+        level: 'info',
+        message: 'title requested'
+    });
+});
+app.get('/routen', function (req, res) {
     res.sendFile(__dirname + '/public/Routen.html');
+    logger.log({
+        level: 'info',
+        message: 'routes requested'
+    });
 });
-app.get('/history', function(req, res) {
+app.get('/history', function (req, res) {
     res.sendFile(__dirname + '/public/history.html');
+    logger.log({
+        level: 'info',
+        message: 'history requested'
+    });
 });
-app.get('/messages', function(req, res) {
+app.get('/messages', function (req, res) {
     res.sendFile(__dirname + '/public/Message.html');
+    logger.log({
+        level: 'info',
+        message: 'messages requested'
+    });
 });
-app.get('/plan', function(req, res) {
+
+app.get('/plan', function (req, res) {
     res.sendFile(__dirname + '/public/fahrtenplan.html');
+    logger.log({
+        level: 'info',
+        message: 'plan requested'
+    });
 });
 
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Server läuft auf Port 3000');
+    logger.log({
+        level: 'info',
+        message: 'Server online'
+    });
 });
